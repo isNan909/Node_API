8dce417a5655b89c70947ececb9c5e020eca144f
Ishan Manandhar <ishan02016@gmail.com>
2020-04-16 13:09:12 +0545

    folder structuring

--- queries/food.sql
+++ queries/food.sql
@@ -1,13 +1,23 @@
+CREATE DATABASE foodie;
+-- /l foodie
+-- /c foodie
+
 CREATE ROLE api_user WITH LOGIN PASSWORD ‘root';
 ALTER ROLE api_user CREATEDB;
 
 
+-- \q
+psql -d postgres -U api_user;
+
+
+
 CREATE TABLE food (
-  ID BIGINT PRIMARY KEY,
+  ID SERIAL PRIMARY KEY,
   Dish VARCHAR(255) NOT NULL,
   Country VARCHAR(255) NOT NULL
 );
 
 
-INSERT INTO food (dish, country) 
-VALUES  (‘Migas', ‘Mexican’), (’Tom Yam',’Thai’);
+INSERT INTO food (dish, country) VALUES  (‘Migas', ‘Mexican’),(’Tom Yam', ’Thai’);
+
+SELECT * FROM food
