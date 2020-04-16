ec2b187153f609468aa2355e11a20de6ff029407
Ishan Manandhar <ishan02016@gmail.com>
2020-04-16 20:40:51 +0545

    edited env file

--- configuration/config.js
+++ configuration/config.js
@@ -3,7 +3,9 @@ require('dotenv').config()
 const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DB_HOST}:${process.env.PORT}/${process.env.DB_DATABASE}`
+
+console.log(connectionString);
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
@@ -11,3 +13,4 @@ const pool = new Pool({
 })
 
 module.exports =  pool ;
+
